e8224ccba882f837277705701551650152d50b57
shirui <1229408499@qq.com>
2018-01-05 19:51:36 +0800

    加入了题目变成xml的脚本

---
+++ exam2xml.py
@@ -0,0 +1,43 @@
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        pass
+
+    try:
+        import unicodedata
+        unicodedata.numeric(s)
+        return True
+    except (TypeError, ValueError):
+        pass
+
+    return False
+
+file = open("51.txt")
+
+while 1:
+    count = 0
+    lines = file.readlines()
+    if not lines:
+        break
+    for i in range(len(lines)):
+        if lines[i][0:1] == '第':
+            if is_number(lines[i][2:3]):
+                str = "<num>" + lines[i][1:3] + "<main>"
+            else:
+                str = "<num>" + lines[i][1:2] + "<main>"
+            str = str + lines[i+1][0:-1] + "<A>" + lines[i+2][2:-1]
+            if lines[i+3][0:1] == 'B':
+                str = str + "<B>" + lines[i+3][2:-1]
+                count = 3
+            if lines[i+4][0:1] == 'C':
+                str = str + "<C>" + lines[i+4][2:-1]
+                count = 4
+            if lines[i+5][0:1] == 'D':
+                str = str + "<D>" + lines[i+5][2:-1]
+                count = 5
+            str = str + "<end>" + lines[i+count+3][3:4] + "</end>"
+            print(str)
+        else:
+            pass
