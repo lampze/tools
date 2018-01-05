0a085436c8b38eb4a33c26cedc6cf4a33d620247
shirui <1229408499@qq.com>
2018-01-05 20:10:13 +0800

    加入了异常处理，和参数传递

--- exam2xml.py
+++ exam2xml.py
@@ -1,3 +1,5 @@
+import sys
+
 def is_number(s):
     try:
         float(s)
@@ -14,30 +16,38 @@ def is_number(s):
 
     return False
 
-file = open("51.txt")
+def main(f):
+    file = open(f)
 
-while 1:
-    count = 0
-    lines = file.readlines()
-    if not lines:
-        break
-    for i in range(len(lines)):
-        if lines[i][0:1] == '第':
-            if is_number(lines[i][2:3]):
-                str = "<num>" + lines[i][1:3] + "<main>"
+    while 1:
+        count = 0
+        lines = file.readlines()
+        if not lines:
+            break
+        for i in range(len(lines)):
+            if lines[i][0:1] == '第':
+                if is_number(lines[i][2:3]):
+                    str = "<num>" + lines[i][1:3] + "<main>"
+                else:
+                    str = "<num>" + lines[i][1:2] + "<main>"
+                str = str + lines[i+1][0:-1] + "<A>" + lines[i+2][2:-1]
+                if lines[i+3][0:1] == 'B':
+                    str = str + "<B>" + lines[i+3][2:-1]
+                    count = 3
+                if lines[i+4][0:1] == 'C':
+                    str = str + "<C>" + lines[i+4][2:-1]
+                    count = 4
+                if lines[i+5][0:1] == 'D':
+                    str = str + "<D>" + lines[i+5][2:-1]
+                    count = 5
+                str = str + "<end>" + lines[i+count+3][3:4] + "</end>"
+                print(str)
             else:
-                str = "<num>" + lines[i][1:2] + "<main>"
-            str = str + lines[i+1][0:-1] + "<A>" + lines[i+2][2:-1]
-            if lines[i+3][0:1] == 'B':
-                str = str + "<B>" + lines[i+3][2:-1]
-                count = 3
-            if lines[i+4][0:1] == 'C':
-                str = str + "<C>" + lines[i+4][2:-1]
-                count = 4
-            if lines[i+5][0:1] == 'D':
-                str = str + "<D>" + lines[i+5][2:-1]
-                count = 5
-            str = str + "<end>" + lines[i+count+3][3:4] + "</end>"
-            print(str)
-        else:
-            pass
+                pass
+
+for i in sys.argv[1:len(sys.argv)]:
+    try:
+        main(i)
+    except:
+        print("没有这个文件！！！")
+
